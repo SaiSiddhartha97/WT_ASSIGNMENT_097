34eef14a423ffe03d107f6ce190fc3c3bd2f7bd2
SaiSiddhartha97 <43650757+SaiSiddhartha97@users.noreply.github.com>
2019-02-15 20:52:15 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,20 @@
+document.getElementById('register').addEventListener('click',function sid() {
+	// body...
+	document.querySelector(".bg-modal").style.display='flex';
+});
+
+document.querySelector('.close2').addEventListener('click',function sid1(){
+	// body...
+	document.querySelector('.bg-modal').style.display='none';
+});
+
+
+document.getElementById('sign-in').addEventListener('click',function sid2() {
+	// body...
+	document.querySelector(".bg-model").style.display='flex';
+});
+
+document.querySelector('.close1').addEventListener('click',function sid3(){
+	// body...
+	document.querySelector('.bg-model').style.display='none';
+});
